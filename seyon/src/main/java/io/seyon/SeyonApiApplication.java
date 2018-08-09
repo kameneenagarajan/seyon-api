a6efc6148c660bec826ed3de7372089c979b0d49
ramalaxmi <ramlaxmi@github.com>
2018-08-09 20:13:36 +0530

    Cloud deploy changes

--- seyon/src/main/java/io/seyon/SeyonApiApplication.java
+++ seyon/src/main/java/io/seyon/SeyonApiApplication.java
@@ -1,12 +1,15 @@
 package io.seyon;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 
 import org.apache.catalina.filters.RemoteAddrFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -15,6 +18,8 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.seyon.invoice.entity.SACCode;
+import io.seyon.invoice.repository.SACCodeRepository;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -23,15 +28,32 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
-public class SeyonApiApplication {
+public class SeyonApiApplication implements CommandLineRunner {
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	SeyonApiProperties seyonProperties;
 	
+	@Autowired
+	SACCodeRepository sacCodeRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SeyonApiApplication.class, args);
 	}
+	
+	 @Override
+	    public void run(String... arg0) throws Exception {
+		 LocalDate date = LocalDate.of(2018, 8, 9);
+		 
+		 SACCode sacCode = new SACCode();
+		 sacCode.setSacCode("SAC1");
+		 sacCode.setCgstPercent(5.0);
+		 sacCode.setSgstPercent(5.0);
+		 sacCode.setIgstPercent(5.0);
+		 sacCode.setStartDate(date.minusDays(30));
+		 sacCode.setEndDate(date);
+		 sacCodeRepository.save(sacCode);
+	    }
 
 	@Bean
 	public Docket productApi() {
